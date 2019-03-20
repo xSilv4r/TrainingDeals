4ed0d6d8abce4d047f0d748cc95bf449b56091ff
xSilv4r <chedi.chered@edu.isetcom.tn>
2019-03-20 21:49:33 +0100

    Added login route

--- server/app.js
+++ server/app.js
@@ -1,14 +1,21 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-const app = express()
-const db = require('./config/keys').mongoURI;
+const login = require("./routes/api/login");
+
+const app = express();
+const db = require("./config/keys").mongoURI;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/api/login", login);
 
 mongoose
-.connect(db,{ useNewUrlParser: true })
-.then(()=>console.log('MongoDB connected'))
-.catch(err => console.log(err))
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
-app.listen(port,()=>console.log(`Server running on ${port}`))
+app.listen(port, () => console.log(`Server running on ${port}`));
